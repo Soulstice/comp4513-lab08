03f5e37a1b285c427f0cf9ff665b076edc2726e7
Ryan Barton <ry-guy-17@hotmail.com>
2016-02-04 00:05:34 +0000

    Finished walkthrough files

--- js/lab08-walkthrough02.js
+++ js/lab08-walkthrough02.js
@@ -1,1 +1,17 @@
 /* code goes here */
+
+/*
+var stories = document.getElementById("stories");
+for (var i=0;i<stories.childNodes.length; i++)
+{
+    //console.log("child " + i + " =" + stories.childNodes[i] +
+    //" nodeType=" + stories.childNodes[i].nodeType);
+    
+    stories.childNodes[i].style.backgroundColor = "green";
+}*/
+
+var nodes = document.querySelectorAll("#stories div");
+
+for (var i=0; i < nodes.length; i++) {
+    nodes[i].style.backgroundColor = "green";
+}
